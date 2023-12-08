a8b5616532baba33e6784794101201937f80c98e
Denis Cohen <denis.cohen.132@gmail.com>
2023-12-08 13:42:52 +0100

    commit package

---
+++ R/poweranalysis.R
@@ -0,0 +1,17 @@
+#' poweranalysis: Power Analysis for Social Science Research
+#'
+#' @description The \code{poweranalysis} package provides a collection of
+#' interactive \code{learnr} materials for the workshop
+#' "Power Analysis for Social Science Research". The workshop consists of
+#' a lecture and a lab. You can call the materials using the code provided
+#' in the examples
+#' below.
+#'
+#' @seealso \code{\link{learnr}}
+#'
+#' @example man/examples/poweranalysis.R
+#'
+#' @docType package
+#' @name poweranalysis
+NULL
+#> NULL
---
+++ man/examples/poweranalysis.R
@@ -0,0 +1,4 @@
+\dontrun{
+  learnr::run_tutorial("01-lec", package = "poweranalysis")
+  learnr::run_tutorial("01-lab", package = "poweranalysis")
+}
